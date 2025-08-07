147232b049093bba2686214aed13a6ff5398e53c
Prince Kumar <princekrcoder@gmail.com>
2025-08-07 22:07:04 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,45 @@ import { Toaster } from "@/components/ui/toaster";
 import { SpaceAnimation } from '@/components/space-animation';
 
 export const metadata: Metadata = {
-  title: 'Prince Kumar | Developer & DSA Enthusiast',
-  description: "Prince Kumar's personal portfolio. A full stack developer and problem solver.",
+  title: "Princefolio - Prince Kumar | Full Stack Developer",
+  description:
+    "Explore Prince Kumar’s portfolio – Full Stack Developer (MERN Stack), DSA enthusiast with 2+ years of experience and 20+ completed freelance projects.",
+  keywords: [
+    "Princefolio",
+    "Prince Kumar",
+    "Princekrcoder",
+    "princekrcoder",
+    "Prince Portfolio",
+    "Prince Web Developer",
+    "Prince Full Stack Developer",
+    "MERN Developer",
+    "Freelance Web Developer",
+    "DSA Expert",
+    "Frontend Developer",
+  ],
+  openGraph: {
+    title: "Princefolio - Prince Kumar | Full Stack Developer",
+    description:
+      "Explore my MERN stack projects, resume, and open-source work. 600+ DSA problems solved and 20+ freelance projects delivered.",
+    url: "https://princefolio-iota.vercel.app",
+    siteName: "Princefolio",
+    images: [
+      {
+        url: "https://princefolio-iota.vercel.app/og-image-v2.png",
+        width: 1200,
+        height: 630,
+        alt: "Princefolio Banner",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Princefolio - Prince Kumar | Full Stack Developer",
+    description:
+      "Visit Prince Kumar’s portfolio website – MERN Developer & DSA Enthusiast with 600+ problems solved.",
+    images: ["https://princefolio-iota.vercel.app/og-image-v2.png"],
+  },
 };
 
 export default function RootLayout({
